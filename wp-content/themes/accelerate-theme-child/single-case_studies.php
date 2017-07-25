51aefa26785c08bea06a0fb178a57ea27387a16c
Kelly Robinson <kshimrob@gmail.com>
2017-07-24 21:47:35 -0500

    added case studies custom post

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,14 @@
+<h2>Current strategy, design, and development</h2>
+Client: MailChimp
+
+&nbsp;
+
+This is a fabulous thing I did for this company. I did a ton of stuff and blah di blah di blah.
+
+&nbsp;
+
+<a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-1.png"><img class="alignnone size-medium wp-image-47335" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-1-300x221.png" alt="" width="300" height="221" /></a> <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2.png"><img class="alignnone size-medium wp-image-47336" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2-223x300.png" alt="" width="223" height="300" /></a> <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3.png"><img class="alignnone size-medium wp-image-47337" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3-150x300.png" alt="" width="150" height="300" /></a>
+
+<a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2.png"><img class="alignnone size-medium wp-image-47336" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2-223x300.png" alt="" width="223" height="300" /></a>
+
+<a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3.png"><img class="alignnone size-medium wp-image-47337" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3-150x300.png" alt="" width="150" height="300" /></a>
