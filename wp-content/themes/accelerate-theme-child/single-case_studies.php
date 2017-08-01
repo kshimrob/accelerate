098494e5416b2a2df8342d38edc5abe9101fbdbe
Kelly Robinson <kshimrob@gmail.com>
2017-07-31 19:01:09 -0500

    updated case study page with fields

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,19 +1,37 @@
 <?php get_header(); ?>
 
+  
+<?php $size = "full"; ?>
+<?php $services = get_field('services'); ?>
+<?php $client = get_field('client'); ?>
+<?php $site_link = get_field('site_link'); ?>
+<?php $description = get_field('description'); ?>
+<?php $image_1 = get_field('image_1'); ?>
+<?php $image_2 = get_field('image_2'); ?>
+<?php $image_3 = get_field('image_3'); ?>
+
+
+
 <div class="case-study">
   <section class="case-study-info">
     <h1 class="case-study-title"><?php the_title(); ?></h1>
-    <h2 class="case-study-subtitle">Current strategy, design, and development</h2>
-    <h2 class="case-study-subtitle">Client: MailChimp</h2>
-    <p> This is a fabulous thing I did for this company. I did a ton of stuff and blah di blah di blah.</p>
+    <h2 class="case-study-subtitle"><?php echo $services; ?></h2>
+    <h2 class="case-study-subtitle">Client: <?php echo $client; ?></h2>
+    <h2 class="case-study-subtitle"><a href="<?php echo $site_link; ?>"><?php echo $site_link; ?></a></h2>
+    <p><?php echo $description; ?></p>
+
   </section>
 
   <section class="case-study-images">
+  <?php if($image_1) {?>
+    <?php echo wp_get_attachment_image( $image_1, $size ); ?>
+  <?php } ?>
+  <?php if($image_2) {?>
+    <?php echo wp_get_attachment_image( $image_2, $size ); ?>
+  <?php } ?>
+  <?php if($image_3) {?>
+    <?php echo wp_get_attachment_image( $image_3, $size ); ?>
+  <?php } ?>
 
-    <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-1.png"><img class="alignnone size-medium wp-image-47335" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-1-300x221.png" alt="" width="300" height="221" /></a> <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2.png"><img class="alignnone size-medium wp-image-47336" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2-223x300.png" alt="" width="223" height="300" /></a> <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3.png"><img class="alignnone size-medium wp-image-47337" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3-150x300.png" alt="" width="150" height="300" /></a>
-
-    <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2.png"><img class="alignnone size-medium wp-image-47336" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-2-223x300.png" alt="" width="223" height="300" /></a>
-
-    <a href="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3.png"><img class="alignnone size-medium wp-image-47337" src="http://localhost:8888/accelerate/wp-content/uploads/2017/07/mc-thumbnail-3-150x300.png" alt="" width="150" height="300" /></a>
   </section>
 </div>
